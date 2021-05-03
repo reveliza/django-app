c0bd1d3b062b9d0c0c20596695cb129a9eb1780a
Eliza <eliza.suhareva@gmail.com>
2021-05-03 21:02:50 +0200

    adding testing

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -8,6 +8,9 @@ class Question(models.Model):
     pub_date = models.DateTimeField('date published')
     def __str__(self):
         return self.question_text
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 class Choice(models.Model):
